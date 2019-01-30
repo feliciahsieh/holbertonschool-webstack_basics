b3cb4a1ef2a483eb40ff9b12cf2bb2703acdfd3d
feliciahsieh <felicia@tixwriteoff.com>
2019-01-30 19:01:36 +0000

    first commit with code

---
+++ 0x01-python_basics/6-replace_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    if type(element) is not int:
+        return my_list
+
+    if idx < 0 or idx > (len(my_list) - 1):
+        return my_list
+
+    my_list[idx] = element
+    return my_list
