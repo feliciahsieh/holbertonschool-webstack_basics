e19e90bbd0b2ceb89905950c7d0caea0fcdf828c
feliciahsieh <felicia@tixwriteoff.com>
2019-01-30 18:54:48 +0000

    first commit with code

---
+++ 0x01-python_basics/5-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import sys
+
+n = len(sys.argv) - 1
+if n == 0:
+    print("0 arguments.")
+elif n == 1:
+    print("1 argument:\n{}: {}".format(n, sys.argv[1]))
+else:
+    print("{} arguments:".format(n))
+    for i in range(1, n + 1):
+        print("{}: {}".format(i, sys.argv[i]))
