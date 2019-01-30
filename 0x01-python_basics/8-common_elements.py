7ffb1212ac3c952f56f82c089155c77b730fce16
feliciahsieh <felicia@tixwriteoff.com>
2019-01-30 19:19:51 +0000

    first commit with code

---
+++ 0x01-python_basics/8-common_elements.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    if type(set_1) is set and type(set_2) is set:
+        common = {}
+        common = set_1.intersection(set_2)
+        return common
+    else:
+        return {}
