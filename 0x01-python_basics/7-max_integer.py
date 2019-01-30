43a6697210156590625aed73e7428c0494f9083e
feliciahsieh <felicia@tixwriteoff.com>
2019-01-30 19:06:04 +0000

    first commit with code

---
+++ 0x01-python_basics/7-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    n = len(my_list)
+    if n == 0:
+        return None
+
+    max = my_list[0]
+    for i in range(1, n):
+        if max < my_list[i]:
+            max = my_list[i]
+    return max
