c4ce82d37acda494aa68acf6984c85ca32a7131d
feliciahsieh <felicia@tixwriteoff.com>
2019-01-31 02:30:26 +0000

    first commit with code

---
+++ 0x01-python_basics/101-print_diagonal.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" 101-print_diagonal.py - draw an ascii diagonal to output
+"""
+
+
+def print_diagonal(n):
+    """
+    print_diagonal - draw an ascii diagonal to output
+    Arguments:
+    n: number of diagonals
+    Returns: N/A
+    """
+    if n <= 0:
+        print("\n")
+    elif n > 0:
+        for i in range(0, n):
+            print("{}\\".format(" " * i))
