df00b5a7e860b68ca3ef9d6c418211e37a8db198
feliciahsieh <felicia@tixwriteoff.com>
2019-01-31 02:05:40 +0000

    first commit with code

---
+++ 0x01-python_basics/102-infinite_add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" 102-infinite_add.py - adds infinite-sized integers together """
+
+import sys
+
+
+if len(sys.argv) < 2:
+    print("0")
+elif len(sys.argv) == 2:
+    print(sys.argv[1])
+else:
+    sum = 0
+    for i in range(1, len(sys.argv)):
+        sum += int(sys.argv[i])
+    print(sum)
