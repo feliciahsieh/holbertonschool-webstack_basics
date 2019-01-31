a0963e2d3c600f7e0d68642a7e62ed7d4d4d68ab
feliciahsieh <felicia@tixwriteoff.com>
2019-01-31 01:19:02 +0000

    first commit with code

---
+++ 0x01-python_basics/106-weight_average.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""106-weight_average.py - calc weighted average of all integer tuples
+"""
+
+
+def weight_average(my_list=[]):
+    """ weight_average() - calc weighted average of all integer tuples
+    Arguments:
+    my_list: list of tuples
+    Returns: weighted average
+    """
+    sum = 0
+    denom = 0
+
+    for i in my_list:
+        sum += i[0] * i[1]
+        denom += i[1]
+
+    return sum / denom
