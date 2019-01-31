81ab6cd98e2de383619910d809d52f7f8a135351
feliciahsieh <felicia@tixwriteoff.com>
2019-01-31 02:16:32 +0000

    added if __name__ == __main__

--- 0x01-python_basics/102-infinite_add.py
+++ 0x01-python_basics/102-infinite_add.py
@@ -4,12 +4,15 @@
 import sys
 
 
-if len(sys.argv) < 2:
-    print("0")
-elif len(sys.argv) == 2:
-    print(sys.argv[1])
-else:
-    sum = 0
-    for i in range(1, len(sys.argv)):
-        sum += int(sys.argv[i])
-    print(sum)
+if __name__ == "__main__":
+   # stuff only to run when not called via 'import' here
+
+    if len(sys.argv) < 2:
+        print("0")
+    elif len(sys.argv) == 2:
+        print(sys.argv[1])
+    else:
+        sum = 0
+        for i in range(1, len(sys.argv)):
+            sum += int(sys.argv[i])
+        print(sum)
