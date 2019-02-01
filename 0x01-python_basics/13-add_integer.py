5e9f6406fda887733b14e5833785c06dac199d7c
feliciahsieh <felicia@tixwriteoff.com>
2019-02-01 03:55:56 +0000

    add more tests

--- 0x01-python_basics/13-add_integer.py
+++ 0x01-python_basics/13-add_integer.py
@@ -11,6 +11,17 @@ def add_integer(a, b):
     b: operand 2
     Returns: raises an rror with a message
     """
+
+    # Check for Infinite number
+    if a == float('inf') or a == -float('inf'):
+        return float('inf')
+    if b == float('inf') or b == -float('inf'):
+        return float('inf')
+
+    # Check for NaN
+    if a != a or b != b:
+        return float('nan')
+
     if type(a) is int or type(a) is float:
         if type(b) is int or type(b) is float:
             return int(a) + int(b)
