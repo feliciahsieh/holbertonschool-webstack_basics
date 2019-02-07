61614cee5e4d11a3cb5cc2b5f333ed47e6cdd5f3
feliciahsieh <felicia@tixwriteoff.com>
2019-02-07 00:37:13 +0000

    first commit with code

---
+++ 0x02-javascript_basics/4-square.js
@@ -0,0 +1,7 @@
+if (isNaN(process.argv[2])) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < parseInt(process.argv[2], 10); i++) {
+    console.log('X'.repeat(parseInt(process.argv[2], 10)));
+  }
+}
