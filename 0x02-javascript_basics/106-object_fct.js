3ff5989334f39a9a71e9985646ceb5fa3c78e8d9
feliciahsieh <felicia@tixwriteoff.com>
2019-02-07 07:35:59 +0000

    first commit with code

---
+++ 0x02-javascript_basics/106-object_fct.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+let myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+
+function incr () {
+  myObject.value += 1;
+}
+myObject.incr = incr;
+
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
