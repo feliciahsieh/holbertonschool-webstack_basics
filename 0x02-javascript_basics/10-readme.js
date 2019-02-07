49fa6c9ae9ebc913cfa32b094f68a30a00fae215
feliciahsieh <felicia@tixwriteoff.com>
2019-02-07 04:09:03 +0000

    remove unneeded try catch

--- 0x02-javascript_basics/10-readme.js
+++ 0x02-javascript_basics/10-readme.js
@@ -1,14 +1,10 @@
 #!/usr/bin/node
 var fs = require('fs');
 
-try {
-  fs.readFile(process.argv[2], 'utf8', function (err, data) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(data);
-    }
-  });
-} catch (error) {
-  console.log(error);
-}
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+  if (err) {
+    return console.log(err);
+  } else {
+    console.log(data);
+  }
+});
