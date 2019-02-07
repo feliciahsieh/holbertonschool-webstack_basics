6e4158bc4f92e39eaa684f88515797921f843175
feliciahsieh <felicia@tixwriteoff.com>
2019-02-07 08:10:26 +0000

    first commit with code

---
+++ 0x02-javascript_basics/108-second_biggest.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+if (process.argv.length === 2) {
+  console.log(0);
+} else if (process.argv.length === 3) {
+  console.log(1);
+} else {
+  let max = parseInt(process.argv[2]);
+  let secondMax = -1;
+
+  for (let i = 3; i <= process.argv.length; i++) {
+    if (max <= parseInt(process.argv[i])) {
+      secondMax = max;
+      max = parseInt(process.argv[i]);
+    } else if (secondMax < parseInt(process.argv[i])) {
+      secondMax = parseInt(process.argv[i]);
+    }
+  }
+  console.log(secondMax);
+}
