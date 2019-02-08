c3141eea1e466cd3f729d1bcb6bef5bb31665f1a
feliciahsieh <felicia@tixwriteoff.com>
2019-02-08 06:02:10 +0000

    doesn't recognize s1.print()

--- 0x02-javascript_basics/109-square.js
+++ 0x02-javascript_basics/109-square.js
@@ -1,6 +1,14 @@
 #!/usr/bin/node
 const Rectangle = require('./107-rectangle').Rectangle;
 
-class Square extends Rectangle {
+exports.Square = function Square (size) {
+  Rectangle.call(this, size, size);
 
-}
+  // inherit parent's prototype property
+  Square.prototype = Object.create(Rectangle.prototype);
+
+  Object.defineProperty(Square.prototype, 'constructor', {
+    value: Square,
+    enumerable: false, // so that it does not appear in 'for in' loop
+    writable: true });
+};
