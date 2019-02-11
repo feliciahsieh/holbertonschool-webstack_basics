7e2dad72f1a030c8d5e9a658eeadf10a5b3c84cf
feliciahsieh <felicia@tixwriteoff.com>
2019-02-11 16:19:21 +0000

    first commit with code

---
+++ 0x03-python_web_scraping/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" 0-hbtn_status.py """
+
+import requests
+
+
+url = "https://intranet.hbtn.io/status"
+r = requests.get(url)
+
+print("Body response:")
+print("    - type: {}".format(type(r.text)))
+print("    - content: {}".format(r.text))
+
+if __name__ == "__main__":)
