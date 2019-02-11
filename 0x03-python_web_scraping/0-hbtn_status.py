e64a64a5e8c408681e590eac95a2763165bb5b5b
feliciahsieh <felicia@tixwriteoff.com>
2019-02-11 16:23:54 +0000

    move guard

--- 0x03-python_web_scraping/0-hbtn_status.py
+++ 0x03-python_web_scraping/0-hbtn_status.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 """ 0-hbtn_status.py """
 
-import requests
-
-
-url = "https://intranet.hbtn.io/status"
-r = requests.get(url)
-
-print("Body response:")
-print("    - type: {}".format(type(r.text)))
-print("    - content: {}".format(r.text))
-
 if __name__ == "__main__":
+
+    import requests
+
+    url = "https://intranet.hbtn.io/status"
+    r = requests.get(url)
+
+    print("Body response:")
+    print("    - type: {}".format(type(r.text)))
+    print("    - content: {}".format(r.text))
