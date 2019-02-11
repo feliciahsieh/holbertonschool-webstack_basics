d13ebccadb4dc1b01999b7fdeb145758751c9f08
feliciahsieh <felicia@tixwriteoff.com>
2019-02-11 17:15:15 +0000

    remove extra white space

--- 0x03-python_web_scraping/2-post_email.py
+++ 0x03-python_web_scraping/2-post_email.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     if len(sys.argv) == 3:
         url = sys.argv[1]
         email = sys.argv[2]
-        payload = { 'email': email }
+        payload = {'email': email}
         r = requests.post(url, data=payload)
         print("{}".format(r.text))
     else:
