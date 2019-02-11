8e0d2dbeca267539f9700afafda924f9e6444665
feliciahsieh <felicia@tixwriteoff.com>
2019-02-11 16:54:07 +0000

    first commit with code

---
+++ 0x03-python_web_scraping/2-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" 2-post_email.py - send POST to url with email and print result """
+
+if __name__ == "__main__":
+
+    import requests
+    import sys
+
+    if len(sys.argv) == 3:
+        url = sys.argv[1]
+        email = sys.argv[2]
+        payload = { 'email': email }
+        r = requests.post(url, data=payload)
+        print("{}".format(r.text))
+    else:
+        print("Please enter a valid URL and email")
