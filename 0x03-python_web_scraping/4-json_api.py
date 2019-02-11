3890700aa39527d1e4ba87fcad422c49e90fb20a
feliciahsieh <felicia@tixwriteoff.com>
2019-02-11 19:02:59 +0000

    first commit with code. Not finished

---
+++ 0x03-python_web_scraping/4-json_api.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3                                                                                                           
+""" 4-json_api.py - sends user input in a POST request and receives JSON data """
+
+if __name__ == "__main__":
+
+    import requests
+    import sys
+
+    url = "http://0.0.0.0:5000/search_user"
+    params = {}
+
+    if len(sys.argv) == 1:
+        params['q'] = ''
+    elif len(sys.argv) == 2:
+        params = {'q': sys.argv[1] }
+    else:
+        print("Usage: [URL] | [URL] [POST value]")
+        sys.exit(0)
+
+    # print("my params: {}".format(params))
+
+    r = requests.post(url, data=params)
+
+    try:
+        resp = r.json()
+        # print("my status code: {}".format(r.status_code))
+        if r.status_code == 200:  # Valid JSON
+            print("[{}] {}".format(resp['id'], resp['name']))
+        elif r.status_code == 204:  # Empty JSON
+        # elif len(r) == 0:  # Empty JSON
+            print("No result")
+        else:  # Invalid JSON
+            print("Not a valid JSON")
+    except:
+        print("Not a valid JSON")
