18ac5dea76eb52c224fce2fd2f2a1eca1dc18204
feliciahsieh <felicia@tixwriteoff.com>
2019-02-13 14:33:54 +0000

    add tab

--- 0x03-python_web_scraping/0-hbtn_status.py
+++ 0x03-python_web_scraping/0-hbtn_status.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
     r = requests.get(url)
 
     print("Body response:")
-    print("- type: {}".format(type(r.text)))
-    print("- content: {}".format(r.text))
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
