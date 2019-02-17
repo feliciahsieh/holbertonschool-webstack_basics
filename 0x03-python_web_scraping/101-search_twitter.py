c505f42e6c12eedee344db16146a7a463686faf6
feliciahsieh <felicia@tixwriteoff.com>
2019-02-17 16:13:11 +0000

    passes linter. Not working yet

--- 0x03-python_web_scraping/101-search_twitter.py
+++ 0x03-python_web_scraping/101-search_twitter.py
@@ -7,8 +7,9 @@ from sys import argv
 
 """
 $ curl --request GET
- --url 'https://api.twitter.com/1.1/search/tweets.json?q=nasa&result_type=popular'
- --header 'authorization: OAuth oauth_consumer_key="consumer-key-for-app",
+ --url 'https://api.twitter.com/1.1/search/tweets.json?q=nasa&result_type=
+popular' --header 'authorization: OAuth oauth_consumer_key=
+"consumer-key-for-app",
  oauth_nonce="generated-nonce", oauth_signature="generated-signature",
  oauth_signature_method="HMAC-SHA1", oauth_timestamp="generated-timestamp",
  oauth_token="access-token-for-authed-user", oauth_version="1.0"'
@@ -28,7 +29,8 @@ if __name__ == "__main__":
 
         header = {}
         header['Authorization'] = 'Basic '.encode('ascii') + encoded
-        header['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8'
+        header['Content-Type'] = 'application/x-www-form-urlencoded;'
+        header['Content-Type'] += 'charset=UTF-8'
         bodytext = 'grant_type=client_credentials'
 
         param = {}
