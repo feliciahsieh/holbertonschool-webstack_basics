99b516d1faf888b6fd8d6397d1b4a9c4fc6726ca
feliciahsieh <felicia@tixwriteoff.com>
2019-02-19 22:26:10 +0000

    first commit with code

---
+++ 0x04-javascript_web_jquery/101-script.js
@@ -0,0 +1,14 @@
+const $ = window.$;
+window.onload = function () {
+  $('DIV#add_item').click(function () {
+    $('.my_list').append('<li>Item</li>');
+  });
+
+  $('DIV#remove_item').click(function () {
+    $('.my_list li:last-child').remove();
+  });
+
+  $('DIV#clear_list').click(function () {
+    $('.my_list li').remove();
+  });
+};
