7701c08a01d7f27a6289c69525b4c5782c224707
feliciahsieh <felicia@tixwriteoff.com>
2019-02-19 23:29:33 +0000

    newer version

--- 0x04-javascript_web_jquery/7-script.js
+++ 0x04-javascript_web_jquery/7-script.js
@@ -1,7 +1,5 @@
 const $ = window.$;
-$(document).ready(function () {
-  let url = 'https://swapi.co/api/people/5/?format=json';
-  $.get(url, function (data, status) {
-    $('<div>' + data['name'] + '</div>').replaceAll('div#character');
-  });
+let url = 'https://swapi.co/api/people/5/?format=json';
+$.get(url, function (data, status) {
+  $('<div>' + data['name'] + '</div>').replaceAll('div#character');
 });
