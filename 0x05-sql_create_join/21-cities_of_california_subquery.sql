817f2aef535bd925f363dea19bf0e668f572ffb6
feliciahsieh <felicia@tixwriteoff.com>
2019-02-21 13:58:07 +0000

    uppercase word FROM

--- 0x05-sql_create_join/21-cities_of_california_subquery.sql
+++ 0x05-sql_create_join/21-cities_of_california_subquery.sql
@@ -1,5 +1,6 @@
 -- 21-cities_of_california_subquery.sql - list CA cities in database
-SELECT cities.id, cities.name from cities, states
+SELECT cities.id, cities.name
+FROM cities, states
 WHERE cities.state_id = states.id
 AND states.name="California"
 ORDER BY cities.id ASC;
