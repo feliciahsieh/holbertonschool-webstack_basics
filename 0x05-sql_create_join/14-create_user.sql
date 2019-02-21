1e8b17f747fb9829cbacc253185142bbf07c6d63
feliciahsieh <felicia@tixwriteoff.com>
2019-02-21 01:50:18 +0000

    first commit with code

--- 0x05-sql_create_join/14-create_user.sql
+++ 0x05-sql_create_join/14-create_user.sql
@@ -1,2 +1,3 @@
 -- 14-create_user.sql - create a user
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1@localhost' IDENTIFIED BY 'user-0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user-0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
