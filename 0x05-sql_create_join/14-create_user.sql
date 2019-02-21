61829c7e577bed66ed2b5212a5e79b2c4f581b91
feliciahsieh <felicia@tixwriteoff.com>
2019-02-21 02:27:13 +0000

    updated username

--- 0x05-sql_create_join/14-create_user.sql
+++ 0x05-sql_create_join/14-create_user.sql
@@ -1,3 +1,3 @@
 -- 14-create_user.sql - create a user
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user-0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
